41fe083f40c390d85a044463431e5f70f707dc5c
Tim Nolet <tim@magnetic.io>
2017-09-18 13:13:41 +0200

    1.2.0

--- config/default.js
+++ config/default.js
@@ -4,7 +4,7 @@ config.bind = '0.0.0.0'
 config.port = 3000
 
 config.version = require('../package').version
-config.backgroundColour = process.env.SIMPLE_SERVICE_BGCOLOR || 'cornflowerblue'
-config.heroText = process.env.SIMPLE_SERVICE_HERO_TEXT || 'This looks great!'
+config.backgroundColour = process.env.SIMPLE_SERVICE_BGCOLOR || 'red'
+config.heroText = process.env.SIMPLE_SERVICE_HERO_TEXT || 'oh my!'
 
 module.exports = config
